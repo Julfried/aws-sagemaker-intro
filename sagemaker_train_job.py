a0cb70aa812f7c58fd4210a8d452e87542054999
Julfried <julian.grimm8@gmail.com>
2024-12-17 17:44:08 +0100

    Add a uniquie job_name

--- sagemaker_train_job.py
+++ sagemaker_train_job.py
@@ -3,15 +3,16 @@ import boto3
 import sagemaker.logs
 from sagemaker.pytorch import PyTorch
 from sagemaker.estimator import Estimator
+from sagemaker.utils import name_from_base
 
 # Create a SageMaker session
 session = boto3.Session(region_name='eu-central-1')
 sagemaker_session = sagemaker.Session(boto_session=session)
 
-# Get Execution role
+# Define the execution role
 role = "arn:aws:iam::008038232967:role/service-role/SageMaker-BP2023_FHDataScientist"
 
-# Define a PyTorch estimator
+# Define a Sagemaker estimator
 estimator = Estimator(
     role=role,
     image_uri="763104351884.dkr.ecr.eu-central-1.amazonaws.com/pytorch-training:2.5.1-cpu-py311-ubuntu22.04-sagemaker",
@@ -23,5 +24,4 @@ estimator = Estimator(
     output_path='s3://ffg-bp/pytorch-mnist',
 )
 
-estimator.fit(wait=False)
-print(estimator.latest_training_job)
+estimator.fit(job_name=name_from_base("mnist-example"))
